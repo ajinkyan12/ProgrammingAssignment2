ce384c0623e2d9143ee8331773b7352e3428087b
ajinkyan12 <48830290+ajinkyan12@users.noreply.github.com>
2019-04-14 13:18:41 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
 ## A pair of functions that cache the inverse of a matrix.
 ## This function creates a special "matrix" object that can cache its inverse.
 
